5d16d8d89d74d6490a5e940543a7cde5550f9c38
Adam Moore <adamoore@gmail.com>
2020-05-28 15:44:56 -0400

    re-organized folders so the structure is more easily understood. added more exercises.

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    page1: `${__dirname}/src/page1.js`,
-    page2: `${__dirname}/src/page2.js`,
-    page3: `${__dirname}/src/page3.js`,
-    page4: `${__dirname}/src/page4.js`,
+    // page1: `${__dirname}/src/page1.js`,
+    // page2: `${__dirname}/src/page2.js`,
+    // page3: `${__dirname}/src/page3.js`,
+    // page4: `${__dirname}/src/page4.js`,
+    page5: `${__dirname}/src/page5.js`,
+    page6: `${__dirname}/src/page6.js`,
   },
   output: {
     path: `${__dirname}/dist`,
